414ef637244758a0b7b677521798658c46b4fda7
PARK_EN_COE#10 <witawatd>
2020-01-15 17:24:20 +0700

    status door freed

--- espdata/espdata.ino
+++ espdata/espdata.ino
@@ -7,7 +7,7 @@ const char* password = "20102553";
 
 #define APPID   "WorkShopLED"
 #define KEY     "tjVAVjlbJHII6nq"
-#define SECRET  "MgBnXJHN74Lm6ISZ3m3oogCSO" 
+#define SECRET  "MgBnXJHN74Lm6ISZ3m3oogCSO"
 #include <ESP8266WiFiMulti.h>
 #define ALIAS   "smartlock"
 ESP8266WiFiMulti WiFiMulti;
@@ -22,127 +22,127 @@ char str[32];
 MicroGear microgear(client);
 
 void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
-    Serial.print("Incoming message -->");
-    msg[msglen] = '\0';
-    Serial.println((char *)msg);
+  Serial.print("Incoming message -->");
+  msg[msglen] = '\0';
+  Serial.println((char *)msg);
 }
 
 void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) {
-    Serial.println("Connected to NETPIE...");
-    microgear.setAlias(ALIAS);
+  Serial.println("Connected to NETPIE...");
+  microgear.setAlias(ALIAS);
 }
 
-void setup(){
+void setup() {
 
 
-    microgear.on(MESSAGE,onMsghandler);
-    microgear.on(CONNECTED,onConnected);
+  microgear.on(MESSAGE, onMsghandler);
+  microgear.on(CONNECTED, onConnected);
 
-    Serial.begin(115200);
-    Serial.println("Starting...");
+  Serial.begin(115200);
+  Serial.println("Starting...");
 
-    if (WiFi.begin(ssid, password)) {
-   while (WiFi.status() != WL_CONNECTED) {
-            delay(500);
-            Serial.print(".");
-        }
+  if (WiFi.begin(ssid, password)) {
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
     }
+  }
 
-    Serial.println("WiFi connected");  
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
-    microgear.init(KEY,SECRET,ALIAS);
-    microgear.connect(APPID);
+  microgear.init(KEY, SECRET, ALIAS);
+  microgear.connect(APPID);
 }
 
-void loop(){
+void loop() {
 
-  
+
   if ((WiFiMulti.run() == WL_CONNECTED)) {
-    
+
     HTTPClient http;
-    
+
     String url = "http://iot.rmu.ac.th/iot/Smartlock/NEtpie.php";
     Serial.println(url);
     http.begin(url); //HTTP
 
-int httpCode = http.GET();
-if (httpCode > 0) {
-  Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-if (httpCode == HTTP_CODE_OK) {
-  String payload = http.getString();
-  Serial.println(payload);
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
 
-   if (microgear.connected()) {
-        Serial.println("connected");
-        microgear.loop();
+        if (microgear.connected()) {
+          Serial.println("connected");
+          microgear.loop();
 
-        if (timer >= 1000) {
-       
-        //String a = "dfsadfsa,nkjdsnfids";
+          if (timer >= 1000) {
+
+            //String a = "dfsadfsa,nkjdsnfids";
 
             Serial.print("Sending -->");
-            
-            
+
+
             //String data = String(payload)+","+String(stat);
             //String Status = payload.substring(83);
             Serial.println(payload);
-            microgear.publish("/door",String(payload));
+            microgear.publish("/door", String(payload));
             status();
             timer = 0;
-        } 
-else timer += 100;
-    }
-    else {
-        Serial.println("connection lost, reconnect...");
-        if (timer >= 5000) {
+          }
+          else timer += 100;
+        }
+        else {
+          Serial.println("connection lost, reconnect...");
+          if (timer >= 5000) {
             microgear.connect(APPID);
             timer = 0;
+          }
+          else timer += 100;
         }
-        else timer += 100;
-    }
-    delay(100);
-}
+        delay(100);
+      }
 
-} else {
-  Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    http.end();
+  }
 }
-  http.end();
-}
-}
-void status(){
+void status() {
   if ((WiFiMulti.run() == WL_CONNECTED)) {
-    
+
     HTTPClient http2;
-    
+
     String url2 = "http://iot.rmu.ac.th/iot/Smartlock/status.php";
     //cam.printDebug(url2);
     http2.begin(url2); //HTTP
 
-int httpCode2 = http2.GET();
-if (httpCode2 > 0) {
-  Serial.printf("[HTTP] GET... code: %d\n", httpCode2);
-if (httpCode2 == HTTP_CODE_OK) {
-  String payload2 = http2.getString();
-  String iddoor = payload2.substring(0,1);
-  String status = payload2.substring(1);
-  //cam.printDebug(payload);
-  Serial.println(iddoor);
-  Serial.println(status);
-  if(status=="Unlock"){
-                    
-      microgear.writeFeed("smartlock","iddoor:"+String(iddoor),"cqyqmXB6TV4z3QD8z855gZlQFivPyeQf");
-   }
-  
-}
-} else {
-  Serial.printf("[HTTP] GET... failed, error: %s\n", http2.errorToString(httpCode2).c_str());
-}
-  http2.end();
-}
+    int httpCode2 = http2.GET();
+    if (httpCode2 > 0) {
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode2);
+      if (httpCode2 == HTTP_CODE_OK) {
+        String payload2 = http2.getString();
+        String iddoor = payload2.substring(0, 1);
+        String status = payload2.substring(1);
+        //cam.printDebug(payload);
+        Serial.println(iddoor);
+        Serial.println(status);
+        if (status == "Unlock") {
+
+          microgear.writeFeed("smartlock", "door123/1[Sister room]:1", "cqyqmXB6TV4z3QD8z855gZlQFivPyeQf");
+        }
+        if (status == "lock") {
+
+          microgear.writeFeed("smartlock", "door123/1[Sister room]:0" + String(iddoor), "cqyqmXB6TV4z3QD8z855gZlQFivPyeQf");
+        }
+
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http2.errorToString(httpCode2).c_str());
+    }
+    http2.end();
   }
-
-
-  
-   
+}
